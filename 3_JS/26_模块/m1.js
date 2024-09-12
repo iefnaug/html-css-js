34a7c8e791a13a6f48ef7822f07be772b4556e61
iefnaug <guanfei1993@gmail.com>
2024-09-12 14:33:26 +0800

    模块

---
+++ 3_JS/26_模块/m1.js
@@ -0,0 +1,8 @@
+let name = 'scott';
+let person = {
+    name: 'John',
+    age: 11
+}
+
+export {name, person}
+export default name;
---
+++ 3_JS/26_模块/m2.js
@@ -0,0 +1,8 @@
+let coffee ={
+    name: 'lucky coffee',
+    taste: 'sugar'
+}
+let num = 10
+
+export {coffee};
+export default num;

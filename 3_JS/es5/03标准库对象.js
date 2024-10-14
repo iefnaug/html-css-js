e2a39c4e6d3f79dbf7c08d0ae6d44ea6d66593d3
iefnaug <guanfei1993@gmail.com>
2024-10-14 14:08:54 +0800

    regex and json

--- 3_JS/es5/03标准库对象.js
+++ 3_JS/es5/03标准库对象.js
@@ -178,3 +178,131 @@ console.log(num.toExponential(3)) //科学计数法，小数点后保留n位
 console.log(num.toPrecision(4)) //保留n位有效数字
 
 console.log((123.66).toLocaleString('zh-Hans-CN', { style: 'currency', currency: 'CNY' }))
+
+
+/**
+ * String对象
+ */
+let str = 'abc'
+let strWrapped = new String('abc')
+console.log(typeof str) //string
+console.log(typeof strWrapped) //object
+console.log(strWrapped.toString(), strWrapped.valueOf())
+for(let k in strWrapped) {
+    console.log(k)
+}
+
+console.log(String.fromCharCode(104, 101, 108, 108, 111)) //hello，参数不能大于0xFFFF
+console.log('abc'.charAt(1), 'abc'.charCodeAt(1))
+console.log('abcde'.slice(1, 3)) //bc
+console.log('abcc'.indexOf('c'), 'abcc'.lastIndexOf('c'))
+
+let s = 'Abc'
+console.log(s.toLocaleLowerCase(), s.toUpperCase())
+
+s = 'cat, bat, sat, fat'
+let regExpMatchArray = s.match('at');
+console.log(regExpMatchArray)
+
+s = 'a|b|c'
+console.log(s.split('|'))
+
+
+/**
+ * Math对象
+ * Math.E：常数e。
+ * Math.LN2：2 的自然对数。
+ * Math.LN10：10 的自然对数。
+ * Math.LOG2E：以 2 为底的e的对数。
+ * Math.LOG10E：以 10 为底的e的对数。
+ * Math.PI：常数π。
+ * Math.SQRT1_2：0.5 的平方根。
+ * Math.SQRT2：2 的平方根。
+ */
+console.log(Math.abs(-123), Math.floor(12.6), Math.ceil(12.3))
+console.log(Math.round(1.25), Math.random())
+
+/**
+ * Date对象
+ */
+let date = Date() //作为普通函数调用，返回string
+console.log(date)
+date = new Date(2025, 0, 1, 0, 0, 0, 0)
+console.log(date.toString())
+console.log(date.toUTCString())
+
+date = Date.parse("2024-01-01T00:00:00.000") //返回number
+console.log(date)
+
+
+/**
+ * 正则表达式
+ * +?：表示某个模式出现1次或多次，匹配时采用非贪婪模式。
+ * *?：表示某个模式出现0次或多次，匹配时采用非贪婪模式。
+ * ??：表格某个模式出现0次或1次，匹配时采用非贪婪模式。
+ */
+let regex = /abc/i //编译时创建，效率高
+regex = new RegExp('abc', 'igm') //执行时创建
+console.log(regex.lastIndex, regex.source)
+console.log(regex.ignoreCase)
+console.log(regex.multiline)
+console.log(regex.global)
+console.log(regex.flags)
+
+let reg = /x/g, target = 'x_x_x'
+console.log(reg.lastIndex)
+reg.test(target) //修改正则对象的属性, true false
+console.log(reg.lastIndex, reg.source)
+reg.test(target)
+console.log(reg.lastIndex)
+
+let r1 = /x./igm, rs = 'xa_xb_y_y'
+console.log(r1.flags)
+let regExpExecArray = r1.exec(rs);
+console.log(regExpExecArray)
+regExpExecArray = r1.exec(rs)
+console.log(regExpExecArray)
+regExpExecArray = r1.exec(rs)
+console.log(regExpExecArray) //null
+regExpExecArray = r1.exec(rs)
+console.log(regExpExecArray) //回到开始
+
+
+let r2 = /a.?/g
+rs = 'abacad'
+let regExpMatchArray1 = rs.match(r2);
+console.log(regExpMatchArray1) //所有匹配的字符串
+let number = rs.search(r2); //返回第一个匹配的位置，没有返回-1
+console.log(number)
+
+console.log('aaa'.replace('a', 'b')) // "baa"
+console.log('aaa'.replace(/a/, 'b')) // "baa"
+console.log('aaa'.replace(/a/g, 'b')) // "bbb"
+
+let str_ = '  #id div.class  ';
+console.log(str_.replace(/^\s+|\s+$/g, '')) //去除前置和后置空格
+
+
+/**
+ * JSON
+ */
+console.log('JSON-----------')
+let jsonObj = {
+    name: 'scott',
+    age: 11
+}
+// console.log(JSON.stringify(jsonObj))
+// console.log(JSON.stringify(jsonObj, null, 2)) //格式化
+// console.log(JSON.stringify(jsonObj, ['name'])) //指定属性返回
+let v = JSON.stringify(jsonObj, (k, v) => {
+    if (k === "age") {
+        return v * 2
+    }
+    if (k === 'name') {
+        return undefined
+    }
+    return v
+})
+console.log(v)
+let jsonStr = "{\"age\":22}\n"
+console.log(JSON.parse(jsonStr))
